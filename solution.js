0dd4598eab3695bcc17c7c195f306a3e8d9feba4
Fajardoreanne <fajardoreanne31@gmail.com>
2026-03-16 17:52:45 +0800

    Problem 5 Private Inventory

--- solution.js
+++ solution.js
@@ -55,3 +55,19 @@ return playerList
 .join(", ");
 
 }
+
+// Problem 5
+class Item{
+
+#discount = 0.1;
+
+constructor(name,price){
+this.name = name;
+this.price = price;
+}
+
+get finalPrice(){
+return this.price - (this.price * this.#discount);
+}
+
+}
