c66ab19395df776e4309a83d565c66f2e7e74eb5
Fajardoreanne <fajardoreanne31@gmail.com>
2026-03-16 17:55:39 +0800

    Problem 6 Robust Division

--- solution.js
+++ solution.js
@@ -71,3 +71,26 @@ return this.price - (this.price * this.#discount);
 }
 
 }
+
+// Problem 6
+function safeDivide(a,b){
+
+try{
+
+if(b === 0){
+throw new Error("Cannot divide by zero");
+}
+
+return a/b;
+
+}catch(error){
+
+return error.message;
+
+}finally{
+
+console.log("Operation attempted");
+
+}
+
+}
